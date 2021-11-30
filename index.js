d5ef6ebd7f51125b90b1bdc3b138ea25b13311e0
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-11-30 12:37:05 -0300

    Add / route

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const crypto = require('./api/crypto');
 
 app.use(express.json({ extended: false}));
-
+app.get('/', (req, res) => {
+  res.send("Welcome")
+});
 app.use('api/crypto', crypto)
 
 const PORT = process.env.PORT || 3000;
