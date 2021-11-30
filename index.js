ab0b2d808c552f5e14a61029b6b6d7665456eced
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-11-30 12:45:43 -0300

    Change CMC request to main file

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const express = require('express');
+const axios = require('axios');
+const https = require('https');
+require('dotenv').config();
 const app = express();
-const crypto = require('./api/crypto');
+
 
 app.use(express.json({ extended: false}));
-app.get('/', (req, res) => {
-  res.send("Welcome")
+router.get('/', (req, res) => {
+  const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+  axios.defaults.httpsAgent = httpsAgent;
+  const headers = {
+    'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY
+  }
+  axios.get('http://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',{headers})
+  .then(response => {
+    res.json(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 });
-app.use('api/crypto', crypto)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
